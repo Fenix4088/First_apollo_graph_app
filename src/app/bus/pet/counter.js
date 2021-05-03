dcb02b1f867772781b38747b40a3e7445e5a82b2
Yehor Pliasov <fenix4088@gmail.com>
2021-05-03 10:34:43 +0200

    available pets count are displayed

--- src/app/bus/pet/counter.js
+++ src/app/bus/pet/counter.js
@@ -1,9 +1,15 @@
 import React from "react";
+import {useQueryAvailablePets} from "./hooks/useQueryAvailablePets";
 
 export const Counter = () => {
+    const {loading, error, data} = useQueryAvailablePets();
+
+    if(loading) return <p>Loading...</p>;
+    if(error) return <p>We have a problem: {error.message}</p>;
+
     return(
         <>
-            <p>Counter</p>
+            <p>Available pets: {data.availablePets}</p>
         </>
     )
 }

0298493cb7321e717897e4119d3e06c4f2190812
Yehor Pliasov <fenix4088@gmail.com>
2021-05-04 11:30:32 +0200

    download schema

--- src/app/bus/customer/hooks/useQueryAllCustomers/useQueryAllCustomers.js
+++ src/app/bus/customer/hooks/useQueryAllCustomers/useQueryAllCustomers.js
@@ -1,9 +1,9 @@
 import { loader } from "graphql.macro";
-import {useQuery} from "@apollo/client";
+import { useQuery } from "@apollo/client";
 
 const queryAllCustomers = loader("./queryAllCustomers.graphql");
 export const useQueryAllCustomers = () => {
-    const {loading, error, data} = useQuery(queryAllCustomers);
+  const { loading, error, data } = useQuery(queryAllCustomers);
 
-    return {loading, error, allCustomers: data && data.allCustomers};
+  return { loading, error, allCustomers: data && data.allCustomers };
 };

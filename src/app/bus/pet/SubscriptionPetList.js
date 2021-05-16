b318838aae8be895d9f637b43183a96452b45664
Yehor Pliasov <fenix4088@gmail.com>
2021-05-16 11:04:52 +0300

    Added all functional befor auth

--- src/app/bus/pet/SubscriptionPetList.js
+++ src/app/bus/pet/SubscriptionPetList.js
@@ -3,16 +3,17 @@ import { useSubscriptionPet } from "./hooks/useSubscriptionCustom/useSubscriptio
 
 export const SubscriptionPetList = () => {
   const { pet } = useSubscriptionPet();
+
   const petJSX = pet && (
     <ul>
-      <li>Name: {pet.pet.name}</li>
-      <li>Name: {pet.pet.id}</li>
+      <li>Name: {pet.name}</li>
+      <li>Name: {pet.id}</li>
     </ul>
   );
   return (
     <>
       <h1>Subscription List</h1>
-        {petJSX}
+      {petJSX}
     </>
   );
 };
